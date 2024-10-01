ce738dec581edb0050dee9dbecae218859300d18
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-10-01 09:29:03 +0200

    correct a typo preventing deletion of the item

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -2,7 +2,7 @@ const { Pokemon } = require('../db/sequelize');
 
 module.exports = (app) => {
     app.delete('/api/pokemons/:id', (req, res) => {
-       Pokemon.findByPk(res.params.id).then(pokemon => {
+       Pokemon.findByPk(req.params.id).then(pokemon => {
         const pokemonDeleted = pokemon;
         Pokemon.destroy({
             where: { id: req.params.id }
