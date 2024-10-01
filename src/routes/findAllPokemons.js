81a78530fb233816a3eb011a8d3685875fe449f3
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-10-01 11:42:48 +0200

    add search query request in url logic

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,7 +2,21 @@ const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name) {
+      const name = req.query.name;
+      return Pokemon.findAll({
+        where: { name: name }
+      })
+      .then(pokemons => {
+        const message = pokemons.length > 1 ? 
+          message = `Il y a ${pokemons.length} pokémons avec le nom ${name}.`
+         : 
+          message = `Il y a ${pokemons.length} pokémon avec le nom ${name}.`;
+        
+        res.json({ message, data: pokemons })
+      });
+    } else {
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +25,6 @@ module.exports = (app) => {
         const message = `La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`
         res.status(500).json({ message, data: error })
       })
+    }
   })
 }
