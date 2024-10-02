fd304eecd9d3663c7b98b742fe34e514bee4b98f
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-10-02 15:26:04 +0200

    put logic of JWT creation into the login route

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,7 @@
 const { User } = require('../db/sequelize')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const privateKey = require ('../auth/private_key')
 
 module.exports = (app) => {
     app.post('/api/login', (req, res) => {
@@ -13,8 +15,13 @@ module.exports = (app) => {
                     const message = 'Le mot de passe est invalide.'
                     return res.status(401).json({ message })  
                 }
+                //JWT
+                const token = jwt.sign(
+                    { id: user.id },
+                    privateKey,
+                    { expiresIn: '24h' })
                 const message = 'Vous êtes connecté.'
-                return res.json({ message, data: user })
+                return res.json({ message, data: user, token })
             })
         })
         .catch(error => {
